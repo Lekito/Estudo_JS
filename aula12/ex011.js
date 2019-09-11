a396e49b04c81c5f312e561fc5dc96e4a78d1755
Alex Ribeiro <alexlekito075@gmail.com>
2019-09-11 15:05:23 -0400

    Brincando com if e else em JS

---
+++ aula12/ex011.js
@@ -0,0 +1,8 @@
+var idade = 18
+if( idade < 16){
+    console.log(`Não vota, pois tem ${idade} anos.`)
+} else if(idade < 18) {
+        console.log(`Voto opcional, pois tem ${idade} anos.`)
+    } else {
+        console.log(`Voto obrigatório! Pois tem ${idade} anos. `)
+       }

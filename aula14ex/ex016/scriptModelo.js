8e1d62dc49bd3d3c0229e02e8f80c482243dc32f
Alex Ribeiro Alves <54374154+Lekito@users.noreply.github.com>
2020-08-17 00:53:24 -0300

    Updade
    Starting to develop the algorithms.

--- aula14ex/ex016/scriptModelo.js
+++ aula14ex/ex016/scriptModelo.js
@@ -1,1 +1,31 @@
-alert('Olá, mundo!')
+
+
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    var resultado = document.getElementById('res')
+    
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        resultado.innerHTML = 'Contando: '
+        let iniCont = Number(ini.value)
+        let fimCont = Number(fim.value)
+        let passoCont = Number(passo.value)
+        for (let c = iniCont; c <= fimCont; c+=passoCont) {
+            resultado.innerHTML += `-> ${c} ` 
+        } 
+    
+    }
+
+}
+
+
+/*
+function contar(){
+    var res = document.getElementById('resultado')
+    res.innerHTML = `Outra mensagem `
+}
+*/

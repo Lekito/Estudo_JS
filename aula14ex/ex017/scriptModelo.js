44ba117499083a61d2b077c5f099f0b58de9bcda
Alex Ribeiro Alves <54374154+Lekito@users.noreply.github.com>
2020-08-19 05:53:20 -0300

    Update

--- aula14ex/ex017/scriptModelo.js
+++ aula14ex/ex017/scriptModelo.js
@@ -1,5 +1,19 @@
 function calcular() {
-    window.alert('Calculou!')
-    //let cal = document.getElementById('calctxt')
+    let cal = document.getElementById('calctxt');
+    //const fimCal = 10;
+    var tabuada = document.getElementById('tabuada')
+    let calculando = Number(cal.value);
+    if(calculando != ' '){
+        for(let cont = 1; cont <= tabuada.length; cont++ ) {
+        
+            let mut = "";
+            mut = cont * calculando
     
+    
+        }
+    } else {
+        window.alert("Digite um número!")
+    }
+    
+
 }

ab244a384c6d161bc14948494276718040f1ec99
Alex Ribeiro Alves <54374154+Lekito@users.noreply.github.com>
2020-08-20 19:54:18 -0300

    Update
    We created instructions to display the multiplication table on the screen. We also corrected the memory error. For always typing a number, a new multiplication table appears.

--- aula14ex/ex017/scriptModelo.js
+++ aula14ex/ex017/scriptModelo.js
@@ -5,10 +5,11 @@ function tabuada() {
     
     if(cal.value.length != ' '){
         let calculando = Number(cal.value);
+        tab.innerHTML = ''  // Comando para apagar a tabuada exitente, para fazer uma nova.
         for(let cont = 1; cont <= fimCal ; cont++ ) {
-            let item = document.createElement('option')
-            item.text = `${cont} X ${calculando} = ${cont*calculando} `
-            tab.appendChild(item)
+            let item = document.createElement('option') // criando um elemento ('option é uma opção do 'select')
+            item.text = `${cont} X ${calculando} = ${cont*calculando} ` 
+            tab.appendChild(item) // Comando para escrever na tela um elemto criado.
         }
     } else {
         window.alert("Por favor! Digite um número!")

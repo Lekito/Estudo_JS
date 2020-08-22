ddd8e4972d60b5ee91f42ffaac47ec6ac704e82e
Alex Ribeiro Alves <54374154+Lekito@users.noreply.github.com>
2020-08-22 00:06:21 -0300

    Update vetornaTela.js

--- aula15/vetornaTela.js
+++ aula15/vetornaTela.js
@@ -2,3 +2,9 @@ let valores = [8, 1, 7, 4, 2, 9]
 for (let cont = 0; cont < valores.length; cont++) {
     console.log(`O vetor na ${cont} é = [${valores[cont]}]`)
 }
+valores.sort();
+console.log(`Com a ordenação o vetor fica assim!`)
+// Nova forma de usar vetor ou objetos.
+for(let cont in valores) {
+    console.log(`O vetor na ${cont} é = [${valores[cont]}]`)   
+}

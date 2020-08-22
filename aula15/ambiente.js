d5c500349072a8838f9868f5b96e59c681d2d9a6
Alex Ribeiro Alves <54374154+Lekito@users.noreply.github.com>
2020-08-21 23:30:44 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,8 +1,10 @@
 let num = [5,3,2,5,4];
 console.log(`Nosso vetor é ${num}.`)
+console.log(`Nosso vetor tem ${num.length} posições.`)
 num[5] = 8 // inserindo um valor dendro do vetor.
 num.push(7) // inserindo um valor sem precisar dizer o indice.
 console.log(num)
 num.length
 num.sort()
 console.log(num)
+console.log(`Agora o vetor tem ${num.length} posições`)

fc50662b26c9e2dd5f610c32c05be1eaf00182cb
Alex Ribeiro Alves <54374154+Lekito@users.noreply.github.com>
2020-08-24 16:18:42 -0300

    Update

--- aula16ex/scriptModelo.js
+++ aula16ex/scriptModelo.js
@@ -1,5 +1,23 @@
+let valor = document.getElementById('addNumber')
+let notasel = document.getElementById('selAnalise')
+
 function adicionar() {
-    alert('Olá, mundo!')
+    let n1 = Number(valor.value);
+    let vetorAdd = []
+    const TAM = 100
+    if(n1 == '') {
+        alert('Valor invalido! Revise o número antes de adicionar!')
+    } else {
+        if(n1 >= 0 && n1 <= 100) {
+            for(let cont in vetorAdd){
+                let item = document.createElement('option')// parei aqui. Vou adicinonar os elementos no select.
+
+            }
+        } else {
+            alert('Valor invalido! Revise o número antes de adicionar!')
+        }
+    }
+    
 }
 
 function finalizar() {

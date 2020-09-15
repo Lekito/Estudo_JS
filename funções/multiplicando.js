da0c9a47048f2a27887ebdc4f7adf1f9829f38dd
Alex Ribeiro Alves <54374154+Lekito@users.noreply.github.com>
2020-09-15 12:42:05 -0300

    Update multiplicando.js

--- funções/multiplicando.js
+++ funções/multiplicando.js
@@ -5,3 +5,6 @@ function square(numero) {
 let numero = 4;
 square(numero);
 console.log(numero);
+
+/* Parâmetros primitivos (como um número) são passados para as funções por valor; o valor é passado para a função, mas se a função altera o valor do parâmetro, esta mudança não reflete globalmente ou na função chamada.
+*/

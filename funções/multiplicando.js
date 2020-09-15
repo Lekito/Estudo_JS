50f410af020cfa8d6e8f3d73b1f832d5abb22f36
Alex Ribeiro Alves <54374154+Lekito@users.noreply.github.com>
2020-09-15 12:21:48 -0300

    Create multiplicando.js

---
+++ funções/multiplicando.js
@@ -0,0 +1,7 @@
+// Função que recebe um número como parametro para fazer a multiplicação, mas não altera esse parametro!
+function square(numero) {
+    return console.log(numero * numero);
+}
+let numero = 4;
+square(numero);
+console.log(numero);

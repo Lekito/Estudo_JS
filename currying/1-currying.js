034b91621dd0c99ae60ec610a31febdfc9bf9d6b
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-01-27 14:27:24 -0300

    Create 1-currying.js
    JavaScript ES6 com currying.

---
+++ currying/1-currying.js
@@ -0,0 +1,12 @@
+/* Nessa função não usamos o currying, pois 
+o 2 no primeiro parametro sempre se repete. 
+*/
+
+function soma(a,b) {
+    return a + b;
+}
+
+soma(2,2);
+soma(2,3);
+soma(2,4);
+soma(2,5);

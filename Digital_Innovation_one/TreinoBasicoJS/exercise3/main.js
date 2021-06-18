40f7c7e623a8749791fec7df69920c794f5bd8f8
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-06-18 20:35:21 -0300

    Create main.js
    Relembrando alguns conceitos básicos de if e else.

---
+++ Digital_Innovation_one/TreinoBasicoJS/exercise3/main.js
@@ -0,0 +1,12 @@
+// Treinado if
+
+//var idade = 17;
+var idade = prompt("Qual é sua idade?");
+if (idade >= 18){
+    console.log(`Você tem ${idade} anos.`);
+    console.log("Você é maior de idade.");
+}
+else{ 
+    console.log(`Você tem ${idade} anos.`);
+    console.log("Você é menor de idade.");
+}

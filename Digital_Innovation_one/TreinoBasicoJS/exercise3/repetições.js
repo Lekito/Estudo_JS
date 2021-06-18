682c7d2c70f8865b3e86cead831bb3e9cab3e8b3
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-06-18 20:37:08 -0300

    Create repetições.js
    Relembrando alguns conceitos básicos looping.

---
+++ Digital_Innovation_one/TreinoBasicoJS/exercise3/repetições.js
@@ -0,0 +1,22 @@
+var data = new Date();
+console.log(data);
+console.log(data.getMonth() + 1); // getMonth conta sempre do 0, por isso, adicionamos + 1
+console.log(data.getMinutes());
+
+/*
+var count;
+for (count = 0; count < 5; count++){
+    console.log(count);
+}
+*    
+
+
+
+/* 
+var cont = 0;
+
+while( cont <= 5){
+    console.log(cont);
+    cont++;
+}
+*/

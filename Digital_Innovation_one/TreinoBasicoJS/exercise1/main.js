e51e7a2d3cc3883de9295b844aec596872ec0a99
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-06-18 20:31:22 -0300

    Create main.js
    Relembrando alguns conceitos básicos de strings.

---
+++ Digital_Innovation_one/TreinoBasicoJS/exercise1/main.js
@@ -0,0 +1,16 @@
+var nome = "Alex Lekito";
+var idade = 29;
+var idade2 = 1;
+var frase = "Japão é o melhor time do mundo!";
+//alert("Relembrando JS");
+alert(`Seja bem vindo novamente ${nome}`);
+//alert (`${nome} tem ${idade} anos.`); // concatenado strings em JS modo avançado
+
+//alert(nome + " tem " + idade + " anos. OK!" ); // concatenado strings em JS modo simples
+alert(idade + idade2);
+console.log(nome);
+console.log(idade +idade2);
+console.log(frase.toUpperCase()); // toUpperCase() transforma uma string toda em letra maiúscula
+console.log(frase.toLowerCase()); // toLowerCase() transforma uma string em toda em letra minúcula
+console.log(frase.replace("Japão","Brasil"));
+//alert(frase.replace("Japão", "Brasil"));

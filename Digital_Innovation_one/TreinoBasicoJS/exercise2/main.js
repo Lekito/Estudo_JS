d22daefa140ea2fee3fed57c7f1ae6db27ab4786
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-06-18 20:32:12 -0300

    Create main.js
    Relembrando alguns conceitos básicos de Array.

---
+++ Digital_Innovation_one/TreinoBasicoJS/exercise2/main.js
@@ -0,0 +1,33 @@
+// Trabalhando com Array e Dicionário
+
+
+// Lista de Dicionários
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+console.log(frutas[1].nome) // imprime uva
+
+/*  // Dicionário
+
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+*/
+
+/*  // Trabalhando Array simples
+
+var lista = ["maça", "pera", "laranja"];
+console.log(lista);
+lista.push("uva"); // adiciona o último elemento no Array
+console.log(lista);
+console.log(lista[0]);
+lista.pop(); // retira o último elemento do Array
+console.log(lista);
+console.log(lista.length); // length conta quantos elementos tem no Array.
+lista.push("banana");
+console.log(lista.reverse()); // muda a ordem da lista 
+console.log(lista.toString()); // Transforma o Array em String
+
+console.log(lista.toString()[0]) // aqui vai imprimir a primeira letra do primeiro elemento do Array
+console.log(lista.join(" - ")); // join muda a foma com vemos um Array impreço
+*/

074ce47860919443a0334bc1f03628d81d8d19a4
Alex Ribeiro Alves <alexlekito075@gmail.com>
2021-06-19 13:45:54 -0300

    Create main.js
    Treinando algumas funcionalidades básicas do JavaScript

---
+++ Digital_Innovation_one/TreinoBasicoJS/UsandoElementosPagina/main.js
@@ -0,0 +1,30 @@
+
+function clicou(){
+     document.getElementById("agradecimento").innerHTML = "<b> Obrigado </b> por clicar!";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado pro clicar!");
+}
+
+function redirecionar(){
+    window.open("https://movimente-se-next.vercel.app/");
+}
+
+function redirecionar2(){
+    window.location.href = "https://movimente-se-next.vercel.app/";
+}
+
+function trocar(elemento){
+    elemento.innerHTML = "Parabéns você conseguiu um avanço!"; // nessa função passamos o elemento como parametro. No código HTML passamos o this como parametro das funções! Assim pademos utilizar as funcionalidades da função em mais de um elemento evitando repetir o código. NÃO PRECISA DE ID OU CLASSE PARA USAR O ELEMENTO!
+
+    //document.getElementById("mousemove").innerHTML = "Vamos avançar mais um passo!"; // AQUI PRECISA USAR Id.
+    //alert("Vamos avançar mais um passo!");
+}
+
+function voltarText(elemento){
+    elemento.innerHTML = "Passe o mouse aqui";
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada!");
+}

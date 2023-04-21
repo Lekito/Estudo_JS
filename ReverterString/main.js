0c5111b3275e1c27f0454e89206e7918183de271
Lekito <alexlekito075@gmail.com>
2023-04-21 05:49:20 -0300

    Reverte uma string

---
+++ ReverterString/main.js
@@ -0,0 +1,12 @@
+function reverterString(palavra) {
+    let novaPalavra = ""
+
+    for (let i = palavra.length - 1; i >= 0; i--) {
+        novaPalavra += palavra[i]
+    }
+    return novaPalavra
+}
+
+console.log(reverterString('Vou ser novo colega de equipe!'))
+
+console.log(reverterString('arara'))

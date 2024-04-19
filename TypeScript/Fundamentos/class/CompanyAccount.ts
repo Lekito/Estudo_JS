c87346e7dd054c8e20a57ae77f9e0ff823db9f0b
Lekito <alexlekito075@gmail.com>
2024-04-19 14:55:42 -0300

    Completa o metodo getLoan

--- TypeScript/Fundamentos/class/CompanyAccount.ts
+++ TypeScript/Fundamentos/class/CompanyAccount.ts
@@ -4,10 +4,16 @@ export class CompanyAccount extends GoAccount {
 
     constructor(name: string, accountNumber: number) {
         super(name, accountNumber)
+
     }
 
-    getLoan = (): void => {
-        console.log('Você pegou um emprestimo!')
+    getLoan = (balance: number): void => {
+        let balanceLoan: number
+        if (this.validateStatus()) {
+            balanceLoan = this.setBalance(balance)
+
+            console.log('Você pegou R$ ' + balance + " de emprestimo. " + this.getName() + " saldo é R$ " + balanceLoan)
+        }
     }
 
     deposit = (): number => {

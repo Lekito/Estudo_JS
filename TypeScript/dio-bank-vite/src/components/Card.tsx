4d6f76eb83838819882174680d8a56abe918d17c
Lekito <alexlekito075@gmail.com>
2024-12-14 16:13:42 -0300

    Update Home and Card

--- TypeScript/dio-bank-vite/src/components/Card.tsx
+++ TypeScript/dio-bank-vite/src/components/Card.tsx
@@ -1,27 +1,10 @@
-import { Input, Box, Center, Text } from "@chakra-ui/react"
-import { login } from "@/services/login"
-import { DButton } from "./DButton"
-import { useState } from "react"
+import { Box } from "@chakra-ui/react"
 
-
-
-export const Card = () => {
-    const [email, setEmail] = useState<string>('')
-
-
+export const Card = ({ children }: any) => {
 
     return (
-        <Box backgroundColor="#ffffff" borderRadius='25px' padding='15px' marginTop="50px" color='#000000'>
-            {/* {userData === null || userData === undefined && <h1 >Loading...</h1>}  */}
-            <Center color='#000000'>
-                <Text textStyle="2xl" fontWeight="semibold">Faça o login </Text>
-            </Center>
-
-            <Input placeholder="email" color='#1a1919' marginTop="30px" value={email} onChange={(event) => setEmail(event.target.value)} />
-            <Input placeholder="Senha" marginTop="30px" />
-            <Center>
-                <DButton onClick={() => login(email)} />
-            </Center>
+        <Box backgroundColor="#ffffff" borderRadius='25px' padding='15px' marginTop="20px" color='#000000'>
+            {children}
         </Box>
 
     )
--- TypeScript/dio-bank-vite/src/pages/Home.tsx
+++ TypeScript/dio-bank-vite/src/pages/Home.tsx
@@ -1,10 +1,27 @@
 import { Card } from "@/components/Card";
+import DButton from "@/components/DButton";
+import { login } from "@/services/login";
+import { Center, Text, Input } from "@chakra-ui/react";
+import { useState } from "react";
 
 
 const Home = () => {
 
+    const [email, setEmail] = useState<string>('')
+
     return (
-        <Card />
+        <Card>
+            {/* {userData === null || userData === undefined && <h1 >Loading...</h1>}  */}
+            <Center color='#000000'>
+                <Text textStyle="2xl" fontWeight="semibold">Faça o login </Text>
+            </Center>
+
+            <Input placeholder="email" color='#1a1919' marginTop="30px" value={email} onChange={(event) => setEmail(event.target.value)} />
+            <Input placeholder="Senha" marginTop="30px" />
+            <Center>
+                <DButton onClick={() => login(email)} />
+            </Center>
+        </Card>
     )
 }
 

2e28470c51bac7cfcf2e86157e7f0a5316ee0d21
Lekito <alexlekito075@gmail.com>
2024-12-17 14:10:02 -0300

    Create button logout

--- TypeScript/dio-bank-vite/src/components/Header/Header.tsx
+++ TypeScript/dio-bank-vite/src/components/Header/Header.tsx
@@ -1,16 +1,35 @@
-import { Center, Text } from '@chakra-ui/react'
+import { Button, Center, Flex, Spacer, Text } from '@chakra-ui/react'
 import { useContext } from 'react'
 import { AppContext } from '../AppContext'
+import { useNavigate } from 'react-router-dom'
 export const Header = () => {
 
-    const context = useContext(AppContext)
-    console.log('Retorno do Header', context)
+    const { isLoggedIn, setIsLoggedIn } = useContext(AppContext)
+    const navigate = useNavigate()
+    const logout = () => {
+        setIsLoggedIn(false)
+        navigate('/')
+    }
 
     return (
-        <Center bg="bg.emphasized" h="50px" >
-            <Text textStyle="xl" fontWeight="medium">
-                Dio Bank
-            </Text>
-        </Center>
+        <Flex bg="bg.emphasized" padding="5px">
+
+            <Center h="50px" >
+                <Text textStyle="xl" fontWeight="medium">
+                    Dio Bank
+                </Text>
+            </Center>
+
+            {
+                isLoggedIn && (
+                    <>
+                        <Spacer />
+                        <Button onClick={() => logout()}>
+                            Sair
+                        </Button>
+                    </>
+                )
+            }
+        </Flex>
     )
 }

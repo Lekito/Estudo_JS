c0bd635c7189de86e381b2fdce1d8dbb2f1c1210
Lekito <alexlekito075@gmail.com>
2024-12-17 14:14:49 -0300

    Update test

--- TypeScript/dio-bank-vite/src/services/login.test.tsx
+++ TypeScript/dio-bank-vite/src/services/login.test.tsx
@@ -5,36 +5,37 @@ import { login } from './login'
 const mockSetIsLoggedIn = vi.fn()
 const mockNavigate = vi.fn()
 
-vi.mock(import("react"), async (importOriginal) => {
-    const actual = await importOriginal()
-    return {
+//Exemplos de mock desestruturando o react
 
-        ...actual,
-        useContext: () => ({
-            setIsLoggedIn: mockSetIsLoggedIn
-        })
-    }
-})
+// vi.mock(import("react"), async (importOriginal) => {
+//     const actual = await importOriginal()
+//     return {
 
-vi.mock(import("react-router-dom"), async (importOriginal) => {
-    const actual = (await importOriginal()) as any
-    return {
-        ...actual,
-        useNavigate: () => mockNavigate
-    }
-})
+//         ...actual,
+//         useContext: () => ({
+//             setIsLoggedIn: mockSetIsLoggedIn
+//         })
+//     }
+// })
+
+// vi.mock(import("react-router-dom"), async (importOriginal) => {
+//     const actual = (await importOriginal()) as any
+//     return {
+//         ...actual,
+//         useNavigate: () => mockNavigate
+//     }
+// })
 describe('login', () => {
     const mockEmail = 'alex@gmail.com'
 
     test('Deve exibir um alert com boas vindas caso seja valido', async () => {
-        await login(mockEmail)
-        expect(mockSetIsLoggedIn).toHaveBeenCalledWith(true)
-        expect(mockNavigate).toHaveBeenCalledWith('/1')
+        const response = await login(mockEmail)
+        expect(response).toBeTruthy()
+
     })
 
     test('Deve exibir um erro caso o email seja inválido', async () => {
-        await login('email@example.com')
-        expect(mockSetIsLoggedIn).not.toHaveBeenCalledWith()
-        expect(mockNavigate).not.toHaveBeenCalledWith()
+        const response = await login('email@example.com')
+        expect(response).toBeFalsy()
     })
 })
